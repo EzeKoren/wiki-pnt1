61495868133267d2597fbb453d68dd00124bd6e8
Uriel <urielszw@gmail.com>
2022-11-27 22:01:37 -0300

    Validaciones

--- tp-grupal/Models/Articulo.cs
+++ tp-grupal/Models/Articulo.cs
@@ -10,13 +10,20 @@ namespace tp_grupal.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int _id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Titulo es un campo obligatorio")]
+        [StringLength(50, ErrorMessage = "Titulo debe tener menos de 50 caracteres")]
+        [MinLength(4, ErrorMessage = "Titulo debe tenes mas de 4 caracteres")]
         public string titulo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Categoria es un campo obligatorio")]
+        [StringLength(50, ErrorMessage = "Categoria debe tener menos de 50 caracteres")]
+        [MinLength(4, ErrorMessage = "Categoria debe tenes mas de 4 caracteres")]
         public string categoria { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Contenido es un campo obligatorio")]
+        [StringLength(1000, ErrorMessage = "Contenido debe tener menos de 1000 caracteres")]
+        [MinLength(20, ErrorMessage = "Contenido debe tenes mas de 20 caracteres")]
         public string contenido { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Imagen es un campo obligatorio")]
+        [Url(ErrorMessage = "Imagen debe ser una URL valida")]
         public string imagen { get; set; }
     }
 }
